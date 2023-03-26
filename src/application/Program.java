f3041dec0314fa6b2303104e42774c6a426282f4
Michel Morais <valentimmichel@gmail.com>
2023-03-26 10:47:01 -0300

    Mapa-Demo2 - comparação de ponteiros

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,31 @@
 package application;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
-import java.util.TreeMap;
+
+import entities.Product;
 
 public class Program {
-
+	
 	public static void main(String[] args) {
-		
-		Map<String, String> cookies = new TreeMap<String, String>();
-		
-		cookies.put("username", "Maria");
-		cookies.put("email", "maria@gmail.com");
-		cookies.put("phone", "997711225");
-		
-		cookies.remove("email");
-		cookies.put("phone", "997711332");
-		
-		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
-		System.out.println("Phone number: " + cookies.get("phone"));
-		System.out.println("Email: " + cookies.get("email"));
-		System.out.println("Size: " + cookies.size());
-		
-		System.out.println("ALL COOKIES:");
-		for(String key : cookies.keySet()) {
-			System.out.println(key + ": " + cookies.get(key));
-		}
-	}
 
+		Locale.setDefault(Locale.US);
+
+		Map<Product, Double> stock = new HashMap<>();
+
+		Product p1 = new Product("Tv", 900.00);
+		Product p2 = new Product("Notebook", 2700.00);
+		Product p3 = new Product("Tablet", 400.00);
+
+		stock.put(p1, 10000.0);
+		stock.put(p2, 20000.0);
+		stock.put(p3, 15000.0);
+
+		Product ps = new Product("Tv", 900.00);
+		// Usando a comparação de ponteiros, já que na minha classe 
+		// personalizada não foi implementado o equals e hashCode.
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+
+	}
 }
